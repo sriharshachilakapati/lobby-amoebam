991ca1be2c726c093827bf7693b00bfc11157e51
Subin Siby <subins2000@gmail.com>
2016-06-13 18:56:22 +0530

    Canvas size fixed

--- src/page/index.php
+++ src/page/index.php
@@ -1,9 +1,14 @@
-<script src="<?php echo $this->u("src/lib/amoebam/amoebam/amoebam.nocache.js", true);?>"></script>
-<script type="text/javascript">
-  lobby.load(function (){
-    var canvas = $("canvas")[0];
-    canvas.width = $("#workspace").width() * .8;
-    canvas.height = $("#workspace").height() * .8;
-    $("canvas").appendTo($("#workspace"));
-  });
-</script>
+<?php
+$this->addStyle("game.css");
+?>
+<div class="valign-wrapper">
+  <script src="<?php echo $this->u("src/lib/amoebam/amoebam/amoebam.nocache.js", true);?>"></script>
+  <script type="text/javascript">
+    lobby.load(function (){
+      var canvas = $("canvas")[0];
+      canvas.width = "";
+      canvas.height = "";
+      $("body canvas:last").addClass("valign").appendTo($("#workspace .valign-wrapper"));
+    });
+  </script>
+</div>
