3a67f003a991303b6fba4bd973f8787134414cac
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-07-13 21:42:27 +0530

    Update and performance fixes

--- src/page/index.php
+++ src/page/index.php
@@ -2,13 +2,17 @@
 $this->addStyle("game.css");
 ?>
 <div class="valign-wrapper">
-  <script src="<?php echo $this->u("src/lib/amoebam/amoebam/amoebam.nocache.js", true);?>"></script>
-  <script type="text/javascript">
+    <script src="<?php echo $this->u("src/lib/amoebam/amoebam/amoebam.nocache.js", true);?>"></script>
+    <script type="text/javascript">
     lobby.load(function (){
-      var canvas = $("canvas")[0];
-      canvas.width = "";
-      canvas.height = "";
-      $("body canvas:last").addClass("valign").appendTo($("#workspace .valign-wrapper"));
+        var canvas = $("canvas")[0];
+        $("body canvas:last").addClass("valign").appendTo($("#workspace .valign-wrapper"));
+
+        // Performance hack, disable error checking since this is a released game
+        window.gl.getError = function()
+        {
+            return gl.NO_ERROR;
+        }
     });
-  </script>
+    </script>
 </div>
